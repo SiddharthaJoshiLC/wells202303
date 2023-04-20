53040381b9429231d2b297e31b2a6f65da77d9ea
Sidd <thesidjoshi@gmail.com>
2023-04-20 15:03:25 +0000

    Function-alized database1.py

--- database1.py
+++ database1.py
@@ -1,24 +1,34 @@
 from sqlalchemy import create_engine
-
+import os
 # URL_DB  = "postgresql://tdi:qKmd8s5ze7WAYV@adventureworks.tditrain.com:5431/wells"
-URL_DB  = os.get_env("URL_DB")
+URL_DB  = os.getenv("URL_DB")
 
 engine=create_engine(URL_DB)
 
 depth_min=2000
 grad_min=0.075
 
-query = f"""
+def query_db(depth_min,grad_min):
+    """blabla"""
+
+    engine=create_engine(URL_DB)
+    
+    query = f"""
     SELECT latitude,longitude,depth,gradient
     FROM wells
     WHERE depth > {depth_min} AND gradient > {grad_min}
-"""
+    """
 
-conn=engine.connect()
+    conn=engine.connect()
 
-results=conn.execute(query).fetchall()
+    results=conn.execute(query).fetchall()
 
-conn.close()
+    conn.close()
 
-print(results)
+    return results
 
+if __name__ == '__main__':
+    depth_min=2000
+    grad_min=0.075
+    print(query_db(depth_min,grad_min))
+    
