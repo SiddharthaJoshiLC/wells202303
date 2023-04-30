a6c65839b70456bbc772bb44ea32449971079f9b
Sidd <thesidjoshi@gmail.com>
2023-04-30 17:45:01 +0000

    Added final version of database.py

---
+++ database.py
@@ -0,0 +1,35 @@
+# Code from TDI lesson by Ana Hocevar.
+
+from sqlalchemy import create_engine, text
+import os
+
+URL_DB = os.getenv("URL_DB")
+    
+def query_db(depth_min, grad_min):
+    """Return wells that fit the search criteria."""
+    
+    engine = create_engine(URL_DB)
+
+    query = text(
+        """
+        SELECT latitude, longitude, depth, gradient
+        FROM wells
+        WHERE depth > :depth_min AND gradient > :grad_min
+        """
+    )
+    with engine.connect() as conn:
+        results = (
+            conn
+            .execute(query, depth_min=depth_min, grad_min=grad_min)
+            .fetchall()
+        )
+
+    return results
+
+if __name__ == '__main__':
+    import sys
+    
+    depth_min = float(sys.argv[1])
+    grad_min = float(sys.argv[2])
+
+    print(query_db(depth_min, grad_min))
